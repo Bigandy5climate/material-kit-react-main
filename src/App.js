717912b27091c092eb3a1928d158ea66268b5def
Samuel Nihoul <snolly@protonmail.com>
2022-03-18 12:34:59 +0100

    no idea (just some branch switching)

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import theme from "assets/theme";
 import Presentation from "layouts/pages/presentation";
 
+import React, { useState } from "react";
+
+import { Navbar, Products, Cart, Checkout } from "./shop/components";
+import { commerce } from "./shop/lib/commerce";
 // Material Kit 2 React routes
 import routes from "routes";
 
@@ -45,20 +49,121 @@ export default function App() {
       }
 
       if (route.route) {
-        return <Route exact path={route.route} element={route.component} key={route.key} />;
+        return (
+          <Route
+            exact
+            path={route.route}
+            element={route.component}
+            key={route.key}
+          />
+        );
       }
 
       return null;
     });
 
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(
+        checkoutTokenId,
+        newOrder
+      );
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Navbar
+        totalItems={cart.total_items}
+        handleDrawerToggle={handleDrawerToggle}
+      />
       <Routes>
         {getRoutes(routes)}
         <Route path="/presentation" element={<Presentation />} />
         <Route path="*" element={<Navigate to="/presentation" />} />
-        {/* <Route path="/" element={<Shop />} /> */}
+
+        <Route exact path="/shop">
+          <Products
+            products={products}
+            onAddToCart={handleAddToCart}
+            handleUpdateCartQty
+          />
+        </Route>
+        <Route exact path="/cart">
+          <Cart
+            cart={cart}
+            onUpdateCartQty={handleUpdateCartQty}
+            onRemoveFromCart={handleRemoveFromCart}
+            onEmptyCart={handleEmptyCart}
+          />
+        </Route>
+        <Route path="/checkout" exact>
+          <Checkout
+            cart={cart}
+            order={order}
+            onCaptureCheckout={handleCaptureCheckout}
+            error={errorMessage}
+          />
+        </Route>
       </Routes>
     </ThemeProvider>
   );
